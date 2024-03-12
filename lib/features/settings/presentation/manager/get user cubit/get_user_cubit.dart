6b4b0bcd581df58ff1692c699abb34d4763158fc
badrafyf77 <badr123afyf@gmail.com>
2024-03-12 18:35:24 +0000

    remove lowding state from get user cubit

--- lib/features/settings/presentation/manager/get user cubit/get_user_cubit.dart
+++ lib/features/settings/presentation/manager/get user cubit/get_user_cubit.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pfe_projet/core/model/user_info_model.dart';
