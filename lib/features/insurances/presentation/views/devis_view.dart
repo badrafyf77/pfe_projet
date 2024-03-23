6cbfdb689f9074edac7d476766cec50955b7b24c
badrafyf77 <badr123afyf@gmail.com>
2024-03-23 14:51:41 +0000

    add devis container && header

--- lib/features/insurances/presentation/views/devis_view.dart
+++ lib/features/insurances/presentation/views/devis_view.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:pfe_projet/core/utils/customs/blur_container.dart';
+import 'package:pfe_projet/core/utils/customs/home_appbar.dart';
 
 class DevisView extends StatelessWidget {
   const DevisView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: AppAppBar(
+        withHeaderAndBackBar: true,
+        title: 'Devis',
+      ),
+      body: DevisBlurContainer(),
+    );
+  }
+}
+
+class DevisBlurContainer extends StatelessWidget {
+  const DevisBlurContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 75,
+      ),
+      child: BlurContainer(
+        height: size.height * .5,
+        width: size.width * .7,
+        opacity: 0.6,
+        blurContainerBody: const DevisBody(),
+      ),
+    );
+  }
+}
+
+class DevisBody extends StatelessWidget {
+  const DevisBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
